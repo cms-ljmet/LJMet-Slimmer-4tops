5af94bfe53e9655e504f5d44bba52b706e34b4b7
Wenyu <wenyu_zhang@brown.edu>
2021-08-24 22:28:52 -0400

    local run

--- step2/testStep2.C
+++ step2/testStep2.C
@@ -6,9 +6,12 @@ using namespace std;
 
 void testStep2(){
   TString inputFile=
-    //"/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2016_Feb2020_4t_080420_step1hadds/nominal/TTToSemiLepton_HT500Njet9_TuneCP5_PSweights_13TeV-powheg-pythia8_tt1b_hadd.root"; 
-   // "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2018_Oct2019_4t_042520_step1hadds/nominal/TTTT_TuneCP5_13TeV-amcatnlo-pythia8_hadd.root" ;
-   "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_10072020_step1hadds/nominal/TTToSemiLepton_HT500Njet9_TuneCP5_PSweights_13TeV-powheg-pythia8_ttjj_hadd.root";
+   "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2016_Jan2021_4t_051321_step1hadds/nominal/ttHTobb_M125_TuneCP5_13TeV-powheg-pythia8_hadd.root";   
+    //"/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2018_Oct2019_4t_10072020_step1hadds/nominal/TTTT_TuneCP5_13TeV-amcatnlo-pythia8_hadd.root" ;
+  // "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_032721_step1hadds/nominal/TTToSemiLepton_HT500Njet9_TuneCP5_PSweights_13TeV-powheg-pythia8_ttcc_hadd.root";
+//  "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_02162021_step1hadds/nominal/SingleElectron_hadd.root";
+// "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_02162021_step1hadds/nominal/TTToHadronic_TuneCP5_PSweights_13TeV-powheg-pythia8_tt1b_hadd.root";
+//  "/mnt/hadoop/store/group/bruxljm/FWLJMET102X_1lep2017_Oct2019_4t_02162021_step1hadds/nominal/TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8_hadd.root";
   TString outputFile="step2_test.root";
 
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/");
